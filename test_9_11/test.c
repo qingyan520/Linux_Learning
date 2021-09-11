ad7951c8051f216fa128018cce2699a72aacd97c
qingyan520 <1627961573@qq.com>
2021-09-11 21:17:34 +0800

    test_9_11

---
+++ test_9_11/test.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+int main()
+{
+  pid_t id=fork();
+  if(id==0)
+  {
+    int count=5;
+    while(count)
+    {
+      printf("I am child, pid:%d, ppid:%d, count:%d\n",getpid(),getppid(),count--);
+    sleep(1);
+    }
+    printf("child exit......\n");
+    exit(-1);
+  }
+  else if(id>0)
+  {
+    while(1)
+    {
+      printf("I am father,pid:%d, ppid:%d\n",getpid(),getppid());
+      sleep(1);
+    }
+  }
+  else
+  {
+    printf("fork fail\n");
+  }
+  return 0;
+}
